449e6b03d1fb7582c7e29350035de8c167ab9ec6
Helder Ferreira <woounn@gmail.com>
2017-01-15 19:45:29 +0000

    add header

--- ws/loginws.php
+++ ws/loginws.php
@@ -10,10 +10,13 @@
 
         $rowcount=mysqli_num_rows($res);
 
+        header('Content-type: application/json');
+
         if ($rowcount==1) {
             while ($r = mysqli_fetch_assoc($res)) {
                 $rows[] = $r;
             }
+
             echo json_encode($rows);
         } else {
             $rows[] = array('id'=>'-1');
