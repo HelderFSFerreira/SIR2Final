5b5617541d918dc48a6e2c3ad45d6508bdfa1aeb
Helder Ferreira <woounn@gmail.com>
2017-01-16 11:50:44 +0000

    add ws for check if username is taken

--- ws/checkUserRegistered.php
+++ ws/checkUserRegistered.php
@@ -1,21 +1,20 @@
 <?php
 
-if (isset($_POST['user']) && $_POST['pass'] && $_POST['name']) {
+if(isset($_POST['user'])) {
     $user = $_POST['user'];
-    $pass = $_POST['pass'];
-    $name = $_POST['name'];
 
     include_once 'connection.php';
 
-    $query = "INSERT INTO users1 (username,password,name)VALUES ('$user','$pass','$name')";
+    $query = "SELECT * FROM users WHERE username='$user'";
     $res = $conn->query($query);
 
-    if($res) {
+    $rowcont=mysqli_num_rows($res);
+
+    if ($rowcont==0) {
         $rows = array('status'=>'1');
     } else {
         $rows = array('status'=>'-1');
     }
-
     header('Content-type: application/json');
     var_dump($rows);
 }
