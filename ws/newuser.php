eb289761bbc0a69b19833016aeacffd753c63280
Helder Ferreira <woounn@gmail.com>
2017-01-16 13:58:28 +0000

    add email column

--- ws/newuser.php
+++ ws/newuser.php
@@ -1,13 +1,14 @@
 <?php
 
-if (isset($_POST['user']) && $_POST['pass'] && $_POST['name']) {
+if (isset($_POST['user']) && $_POST['pass'] && $_POST['name'] && $_POST['email']) {
     $user = $_POST['user'];
     $pass = $_POST['pass'];
     $name = $_POST['name'];
+    $email = $_POST['email'];
 
     include_once 'connection.php';
 
-    $query = "INSERT INTO users1 (username,password,name)VALUES ('$user','$pass','$name')";
+    $query = "INSERT INTO users1 (username,password,name,email)VALUES ('$user','$pass','$name','$maill')";
     $res = $conn->query($query);
 
     if($res) {
