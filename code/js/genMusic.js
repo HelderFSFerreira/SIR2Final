3ecdb18f7570a74ec61a6d3586621e57eca9f07a
Helder Ferreira <woounn@gmail.com>
2017-01-31 17:27:27 +0000

    add new shared user to table

--- code/js/genMusic.js
+++ code/js/genMusic.js
@@ -236,8 +236,7 @@ $('#addUserShare').click(function () {
                 username: $('#userToShare').val(),
             },
             success: function(data) {
-                console.log($('#userToShare').val());
-                console.log(data);
+                refillTableUsersShared();
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 console.log($('#userToShare').val());
@@ -286,7 +285,6 @@ function removeSharedUser(usrid) {
             userID: usrid,
         },
         success: function(data) {
-            $("#tableUsersSharedPlaylist > tbody").html("");
             refillTableUsersShared();
         },
         error: function (xhr, ajaxOptions, thrownError) {
@@ -305,6 +303,7 @@ function refillTableUsersShared() {
             playlistID: playlist,
         },
         success: function(data) {
+            $("#tableUsersSharedPlaylist > tbody").html("");
             if (data.status==1) {
                 fillTableUsersShared(data.users);
             }
